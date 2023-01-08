4078438fdebf4ce6df413138cbbd47aa7d383d66
pfsjdsn <canoecanoe@aliyun.com>
2023-01-08 16:58:21 +0800

    post 请求

--- post.js
+++ post.js
@@ -1,5 +1,15 @@
 let http = require('http')
-let url = require('url')
+// querystring url查询参数的解析
+let querystring = require('querystring')
 http.createServer((req, res) => {
-
+    let result = []
+    // req.on('data',function(data){}) 监听当有一段数据到达的时候，回调函数参数data为每段达到的数据。
+    req.on('data', buffer => {
+        result.push(buffer)
+    })
+    // req.on('end',function(){}) 数据全部到达
+    req.on('end', () => {
+        let data = Buffer.concat(result).toString()
+        console.log(querystring.parse(data));
+    })
 }).listen(8888) 
